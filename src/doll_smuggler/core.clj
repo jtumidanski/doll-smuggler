e56a799383ed57775ff60c98e567937d804f884b
jtumidanski <jtumidanski@gmail.com>
2015-08-22 20:42:19 -0400

    Recursive function defined for 0/1 knapsack problem. Code cleanup, polished output, and adding more tests remains.

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -1,7 +1,88 @@
-(ns doll-smuggler.core
-  (:gen-class))
+(ns doll-smuggler.core)
+(use '[clojure.java.io])
+(use '[clojure.string :only (join split)])
+
+(defstruct doll :name :weight :value)
+
+; Read the list of dolls from the path. Place each doll record in a struct in a vector.
+(defn get-dolls [path delimiter]
+  (vec (let [doll-properties
+    (let [lines (line-seq (reader path))]
+      (map (fn [line] (split line delimiter)) lines))]
+    (map #(apply struct doll %) doll-properties))))
+
+; Forward declare for memoization function.
+(declare memoized-compute-solution)
+
+; Allow parsing of string to int. If the string provided is nil, return 0.
+(defn parse-int [string]
+  (cond
+    ; If the string supplied is nil. Return 0.
+    (nil? string) 0
+    ; TODO more validation necessary?
+    ; Otherwise, this looks good. Try and parse the number.
+    :else
+    (Integer/parseInt (re-find #"\A-?\d+" string))
+    ))
+
+; Compute the total value of the dolls provided.
+(defn total-doll-value
+  ; If only a list of dolls is supplied. Assume they want the value of all dolls in the list.
+  ([dolls] (total-doll-value dolls (dec (count dolls))))
+  ; If a list of dolls and an index is supplied. Tally doll value starting at the index and go to zero.
+  ([dolls doll-index]
+   (cond
+     ; If supplied doll-index is < 0 then return 0.
+     (< doll-index 0) 0
+     ; If doll-index is <= 0 then return 0.
+     (<= (count dolls) 0) 0
+     ; If supplied doll-index is >= the number of elements in dolls this is unacceptable. Try with the maximum
+     ; doll-index allowed. (0 based).
+     (>= doll-index (count dolls)) (total-doll-value dolls (dec doll-index))
+     :else
+     ; Otherwise, take the value of the doll at the index, and sum with the doll at doll-index - 1.
+     (+ (let [{doll-value :value} (get dolls doll-index)] (parse-int doll-value))
+        (total-doll-value dolls (dec doll-index))))))
+
+(defn compute-solution
+  ([weight dolls] (compute-solution weight dolls (dec (count dolls))))
+  ([weight dolls doll-index]
+  (cond
+    ; If doll-index is < 0 then we are done. Return an empty list.
+    (< doll-index 0) []
+    ; If weight remaining to assign is 0 then we are done. Return an empty list.
+    (= weight 0) []
+    ; Otherwise, some more recursion is necessary.
+    :else
+      (let [{doll-weight :weight doll-value :value} (get dolls doll-index)]
+        (cond
+          ; If the doll-weight is > the weight remaining the last item solution for this weight is optimal.
+          (> (parse-int doll-weight) weight) (memoized-compute-solution weight dolls (dec doll-index))
+          ; Otherwise, this new item MAY be a part of the optimal solution. Compute two lists. old-list is the
+          ; optimal solution considering the last item solution for this weight. The new-list is optimal solution for
+          ; the remaining weight if we think the current item is part of the optimal solution.
+          :else
+            (let [old-list (memoized-compute-solution weight dolls (dec doll-index))
+                  new-list (memoized-compute-solution (- weight (parse-int doll-weight)) dolls (dec doll-index))]
+              (cond
+                ; If the current item + the optimal solution for the remaining weight is less optimal than the
+                ; old-list, use the old-list.
+                (<= (+ (total-doll-value new-list) (parse-int doll-value)) (total-doll-value old-list)) old-list
+                :else
+                ; We have found a MORE optimal solution! Conjoin the current item with the optimal solution for the
+                ; remaining weight.
+                (conj new-list (get dolls doll-index))
+                )))))))
+
+; Use memoization to cache input against a map of previous inputs for compute-solution.
+(def memoized-compute-solution (memoize compute-solution))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [max-carry-weight]
+  ; Get the list of doll-set-1 from resources/doll-set-1. Use whitespace as a delimiter.
+  ;(get-dolls "resources/doll-set-1" #"\W+")
+  )
+
+(let [dolls (get-dolls "resources/doll-set-1" #"\W+")]
+  (let [solution (compute-solution 400 dolls)]
+    solution))
--- test/doll_smuggler/core_test.clj
+++ test/doll_smuggler/core_test.clj
@@ -2,6 +2,59 @@
   (:require [clojure.test :refer :all]
             [doll-smuggler.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+; Test the get-dolls function against the test input 0 which is an empty file.
+(deftest get-dolls-test-input-0
+  (testing "Population of dolls for doll-set-0"
+    (def solution '())
+    (is (= (get-dolls "resources/doll-set-0" #"\W+") solution))))
+
+; Test the get-dolls function against the test input 1.
+(deftest get-dolls-test-input-1
+  (testing "Population of dolls for doll-set-1"
+    (def solution '({:name "luke", :weight "9", :value "150"}
+                     {:name "anthony", :weight "13", :value "35"}
+                     {:name "candice", :weight "153", :value "200"}
+                     {:name "dorothy", :weight "50", :value "160"}
+                     {:name "puppy", :weight "15", :value "60"}
+                     {:name "thomas", :weight "68", :value "45"}
+                     {:name "randal", :weight "27", :value "60"}
+                     {:name "april", :weight "39", :value "40"}
+                     {:name "nancy", :weight "23", :value "30"}
+                     {:name "bonnie", :weight "52", :value "10"}
+                     {:name "marc", :weight "11", :value "70"}
+                     {:name "kate", :weight "32", :value "30"}
+                     {:name "tbone", :weight "24", :value "15"}
+                     {:name "tranny", :weight "48", :value "10"}
+                     {:name "uma", :weight "73", :value "40"}
+                     {:name "grumpkin", :weight "42", :value "70"}
+                     {:name "dusty", :weight "43", :value "75"}
+                     {:name "grumpy", :weight "22", :value "80"}
+                     {:name "eddie", :weight "7", :value "20"}
+                     {:name "tory", :weight "18", :value "12"}
+                     {:name "sally", :weight "4", :value "50"}
+                     {:name "babe", :weight "30", :value "10"}))
+    (is (= (get-dolls "resources/doll-set-1" #"\W+") solution))))
+
+; Test the get-dolls function against the test input 2.
+(deftest get-dolls-test-input-2
+  (testing "Population of dolls for doll-set-2"
+    (def solution '({:name "justin", :weight "4", :value "12"}
+                     {:name "david", :weight "6", :value "10"}
+                     {:name "kristen", :weight "5", :value "8"}
+                     {:name "edward", :weight "7", :value "11"}
+                     {:name "pauline", :weight "3", :value "15"}
+                     {:name "nathalie", :weight "1", :value "7"}
+                     {:name "frank", :weight "6", :value "9"}))
+    (is (= (get-dolls "resources/doll-set-2" #"\W+") solution))))
+
+; Test the get-dolls function against the test input 3 which is missing a field.
+(deftest get-dolls-test-input-3
+  (testing "Population of dolls for doll-set-3"
+    (def solution '({:name "justin", :weight "4", :value nil}
+                     {:name "david", :weight "6", :value "10"}
+                     {:name "kristen", :weight "5", :value "8"}
+                     {:name "edward", :weight "7", :value "11"}
+                     {:name "pauline", :weight "3", :value "15"}
+                     {:name "nathalie", :weight "1", :value "7"}
+                     {:name "frank", :weight "6", :value "9"}))
+    (is (= (get-dolls "resources/doll-set-3" #"\W+") solution))))
